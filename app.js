7970b2e8ac439d6c29ebcf31f43efb794b609ccf
Alhassan <alhassannartor@gmail.com>
2022-10-30 20:24:12 +0100

    working on challenge-2

---
+++ app.js
@@ -0,0 +1,7 @@
+
+/* YOU MIGHT WANT TO GO BACK ON FRONTEND SIMPLIFIED, TO RESUSCITATE YOUR MEMORY */
+function falsyOrTruthy(elem1, elem2) {
+    return (!elem1) ? elem1 : elem2
+}
+
+console.log(falsyOrTruthy(0, 5))
